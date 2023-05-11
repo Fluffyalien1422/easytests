2b5072959c6f237d705d865f7df545a790bb61fb
Fluffyalien1422 <shawnbhumbla2@gmail.com>
2023-05-10 21:14:05 -0700

    ts compiling fixes + rename types/types

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,9 @@ import * as vm from "vm";
 import * as fs from "fs";
 import * as path from "path";
 import * as api from "./api.js";
-import * as babelParser from "@babel/parser";
-import babelGenerate from "@babel/generator";
+import babel from "@babel/core";
 
-function getSourceFromNodeModule(specifier: string, basePath = "") {
+function getMainPathFromNodeModule(specifier: string, basePath = "") {
   let modPath = path.join(basePath, "node_modules", specifier);
 
   if (fs.lstatSync(modPath).isDirectory()) {
@@ -20,7 +19,17 @@ function getSourceFromNodeModule(specifier: string, basePath = "") {
     }
   }
 
-  return fs.readFileSync(modPath, "utf8");
+  return modPath;
+}
+
+function compileWithBabel(filePath: string) {
+  const res = babel.transformFileSync(filePath, {
+    presets: ["@babel/preset-typescript"],
+  });
+
+  if (!res?.code) throw new Error(`Couldn't compile \`${filePath}\``);
+
+  return res.code;
 }
 
 export async function executeInTestContext(
@@ -29,54 +38,36 @@ export async function executeInTestContext(
     html?: string;
     jsdomOptions?: jsdom.ConstructorOptions;
     basePath?: string;
-    babelPlugins?: babelParser.ParserPlugin[];
   } = {}
 ) {
-  options.babelPlugins ||= [];
-
-  const tsExtensions = [".ts", ".mts"];
-  const fileExt = path.extname(filePath);
-
-  if (![".js", ".mjs", ...tsExtensions].includes(fileExt))
+  if (![".js", ".mjs", ".ts", ".mts"].includes(path.extname(filePath)))
     throw new Error(
       "Test file must be a JavaScript module (`.js` or `.mjs`) or a TypeScript module (`.ts` or `.mts`)."
     );
-
-  if (
-    tsExtensions.includes(fileExt) &&
-    !options.babelPlugins.includes("typescript")
-  )
-    options.babelPlugins.push("typescript");
 
   const dom = new jsdom.JSDOM(options.html, options.jsdomOptions);
   dom.window.$EASYTESTS = api;
   const context = vm.createContext(dom.window);
 
-  const mod = new vm.SourceTextModule(
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    babelGenerate.default(
-      babelParser.parse(fs.readFileSync(filePath, "utf8"), {
-        sourceType: "module",
-        plugins: options.babelPlugins,
-      }),
-      { retainLines: true }
-    ).code,
-    {
-      context,
-      identifier: filePath,
-    }
-  );
+  const mod = new vm.SourceTextModule(compileWithBabel(filePath), {
+    context,
+    identifier: filePath,
+  });
 
   await mod.link((specifier) => {
     let source: string;
 
     if (specifier.endsWith(".d.ts")) source = "";
     else if (specifier.startsWith("./"))
-      source = fs.readFileSync(path.join(filePath, specifier), "utf8");
-    else source = getSourceFromNodeModule(specifier, options.basePath);
+      source = compileWithBabel(path.join(filePath, specifier));
+    else
+      source = compileWithBabel(
+        getMainPathFromNodeModule(specifier, options.basePath)
+      );
 
     return new vm.SourceTextModule(source, {
       context,
+      identifier: specifier,
     });
   });
 
